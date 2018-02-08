22872443a03ef753676917f270d8133f720abdc6
Shawn Medero <soypunk@users.noreply.github.com>
2018-02-07 21:28:34 -0800

    initial commit
    port of Panic's Terminal color scheme for use in Blink shell.

---
+++ panic.js
@@ -0,0 +1,27 @@
+black       = '#111c2a';
+red         = '#f82939'; // red
+green       = '#3bea2a'; // green
+yellow      = '#fcc146'; // yellow
+blue        = '#2d4562'; // blue
+magenta     = '#6f4dbc'; // pink
+cyan        = '#63cce3'; // cyan
+white       = '#838c95'; // light gray
+lightBlack  = '#454640'; // medium gray
+lightRed    = '#f82939'; // red
+lightGreen  = '#3bea2a'; // green
+lightYellow = '#fcc146'; // yellow
+lightBlue   = '#2d4562'; // blue
+lightMagenta= '#6f4dbc'; // pink
+lightCyan   = '#63cce3'; // cyan
+lightWhite  = '#838c95'; // white
+
+t.prefs_.set('color-palette-overrides',
+                 [ black , red     , green  , yellow,
+                  blue     , magenta , cyan   , white,
+                  lightBlack   , lightRed  , lightGreen , lightYellow,
+                  lightBlue    , lightMagenta  , lightCyan  , lightWhite ]);
+
+t.prefs_.set('cursor-color', 'rgba(0, 0, 0, 0.5)');
+t.prefs_.set('cursor-blink', true);
+t.prefs_.set('foreground-color', '#f6f7ee');
+t.prefs_.set('background-color', '#111c2a');
